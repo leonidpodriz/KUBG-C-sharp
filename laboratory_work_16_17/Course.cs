e6ec7489871035205499d58d87c50045c26bf669
Leonid Podriz <leonidpodriz@gmail.com>
2020-10-07 12:03:48 +0300

    Add project files.

---
+++ laboratory_work_16_17/Course.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laboratory_work_16_17
+{
+    class Course
+    {
+        String name, time;
+        int price, hours, level;
+
+        public Course()
+        {
+            this.price = 0;
+            this.hours = 0;
+            this.level = 0;
+            this.name = "Untitled";
+            this.time = "00:00";
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setTime(String time)
+        {
+            this.time = time;
+        }
+
+        public void setPrice(int price)
+        {
+            this.price = price;
+        }
+
+        public void setLevel(int level)
+        {
+            this.level = level;
+        }
+
+        public void setHours(int hours)
+        {
+            this.hours = hours;
+        }
+
+        public Course(String name, String time, int price, int level, int hours)
+        {
+            this.price = price;
+            this.hours = hours;
+            this.level = level;
+            this.name = name;
+            this.time = time;
+        }
+
+        public int calculate_price()
+        {
+            return price * hours;
+        }
+
+        public static bool operator ==(Course obj1, Course obj2)
+        {
+            if (obj1.calculate_price() == obj1.calculate_price())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Course obj1, Course obj2)
+        {
+            if (obj1.calculate_price() == obj1.calculate_price())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} : {calculate_price()} UAH at {time}";
+        }
+
+        public bool Equals(Course obj)
+        {
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return price.Equals(obj.price)
+                   && name.Equals(obj.name)
+                   && level.Equals(obj.level)
+                   && hours.Equals(obj.hours);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Course);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = name.GetHashCode();
+                hashCode = (hashCode * 397) ^ price.GetHashCode();
+                hashCode = (hashCode * 397) ^ hours.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
---
+++ laboratory_work_16_17/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace laboratory_work_16_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course_1 = new Course();
+            Course course_2 = new Course("Python Starter", "19:00", 200, 0, 3);
+            Course course_2_duplicate = new Course("Python Starter", "19:00", 200, 0, 3);
+            Course course_3 = new Course("C# Advanced", "18:00", 500, 3, 3);
+
+            Console.WriteLine(course_1.ToString());
+            Console.WriteLine(course_2.ToString());
+            Console.WriteLine(course_3.ToString());
+            Console.WriteLine($"Equals [true]: {course_2_duplicate == course_2}");
+            Console.WriteLine($"Equals [false]: {course_2_duplicate != course_2}");
+            Console.WriteLine($"Equals [true]: {course_2_duplicate != course_3}");
+
+            Course[] courses = new Course[3];
+
+            for (int i = 0; i < courses.Length; i++)
+            {
+                courses[i] = new Course();
+                courses[i].setName($"Course {i}");
+                courses[i].setTime($"{i}:{i / 100 * 60}");
+                courses[i].setPrice(i + 1 * 100);
+                courses[i].setLevel(i);
+                courses[i].setHours(i * 2);
+            }
+
+            Console.WriteLine("Search by calculated price. Enter price (< or ==)");
+            int priceFromUser = Int16.Parse(Console.ReadLine());
+
+            foreach(Course course in courses)
+            {
+                if (course.calculate_price() < priceFromUser)
+                {
+                    Console.WriteLine($"Found: {course.ToString()}");
+                }
+            }
+        }
+    }
+}
